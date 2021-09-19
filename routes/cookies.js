fc9b0b08c9f804d54e60f0d85bb01d17b562a634
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-09-19 00:08:15 -0700

    JAG - setting cookie on /login

---
+++ routes/cookies.js
@@ -0,0 +1,28 @@
+/*
+ * Route dealing with cookies setting is defined here
+ * Since this file is loaded in server.js into /login, this route handle a GET request to the /login
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    let query = `SELECT id FROM users LIMIT 1`;
+    console.log(query);
+    db.query(query)
+    .then(data => {
+      const cookies = data.rows;
+        res.cookie('user_id', cookies[0].id);
+        res.redirect('categories');
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
