5efb8df44fa8a6894751e0120072ccb168be4fa5
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-09-21 11:16:28 -0700

    Change to user table schema and activities table seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,5 +1,5 @@
-insert into users (first_name, last_name, email, password, created_at) values ('King', 'Adams', 'kadams0@cnn.com', 'kBCkshDU5e0', '2021-05-31');
-insert into users (first_name, last_name, email, password, created_at) values ('Polina', 'Skrobot', 'pskrobot@pcworld.com', 'gnq4WMi', '2021-08-23');
-insert into users (first_name, last_name, email, password, created_at) values ('Jose', 'Bautista', 'jbautista@ocn.ne.jp', 'cSOxqFcnJ5w', '2020-09-24');
-insert into users (first_name, last_name, email, password, created_at) values ('Jairo', 'Aguirre', 'jaguirre@oakley.com', 'fnHuRugx2Q', '2021-03-13');
-insert into users (first_name, last_name, email, password, created_at) values ('Remy', 'Dannohl', 'rdannohl4@barnesandnoble.com', '97kH3LlAYHr', '2020-12-23');
+insert into users (email, password, created_at) values ('kadams0@cnn.com', 'kBCkshDU5e0', '2021-05-31');
+insert into users (email, password, created_at) values ('pskrobot@pcworld.com', 'gnq4WMi', '2021-08-23');
+insert into users (email, password, created_at) values ('jbautista@ocn.ne.jp', 'cSOxqFcnJ5w', '2020-09-24');
+insert into users (email, password, created_at) values ('jaguirre@oakley.com', 'fnHuRugx2Q', '2021-03-13');
+insert into users (email, password, created_at) values ('rdannohl4@barnesandnoble.com', '97kH3LlAYHr', '2020-12-23');
