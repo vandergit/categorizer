b2a1f6ff8060ab5a2c27f4987afa40dea84e9604
Jose Bautista <Cooldj1020@gmail.com>
2021-09-21 23:59:54 -0600

    updating a working branch

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,13 +12,13 @@ draggables.forEach((draggable) => {
     // console.log("!!!!!!!!",draggable.parentElement.classList);
     let user = draggable.id;
     let newVal = 0;
-    if (draggable.parentElement.classList[1] === 'books') {
+    if (draggable.parentElement.classList[1] === "books") {
       newVal = 3;
-    } else if (draggable.parentElement.classList[1] === 'movies') {
+    } else if (draggable.parentElement.classList[1] === "movies") {
       newVal = 1;
-    } else if (draggable.parentElement.classList[1] === 'restaurants') {
+    } else if (draggable.parentElement.classList[1] === "restaurants") {
       newVal = 2;
-    } else if (draggable.parentElement.classList[1] === 'things') {
+    } else if (draggable.parentElement.classList[1] === "things") {
       newVal = 4;
     }
     // console.log("NEEEEEW",newVal, user);
@@ -45,33 +45,35 @@ ulContainers.forEach((ulContainer) => {
 
 const editItem = (newVal, user) => {
   // console.log('!!!!!!!!!!!!!!!!!!!!', newVal);
-  $.ajax({ type: "PUT", url: "/categories/edit" , data: {category_id: newVal,
-    item_id: user}})
-    .then((res) => {
-      console.log("this is the response from app=>>>>");
-    }
-    );
+  $.ajax({
+    type: "PUT",
+    url: "/categories/edit",
+    data: { category_id: newVal, item_id: user },
+  }).then((res) => {
+    console.log("this is the response from app=>>>>");
+  });
 };
 
 const deleteItem = (activity) => {
-  $.ajax({ type: "POST", url: "/categories/delete" , data: {activity_id: activity}})
-    .then((res) => {
-      console.log("this is a deletion");
-    }
-    );
+  $.ajax({
+    type: "POST",
+    url: "/categories/delete",
+    data: { activity_id: activity },
+  }).then((res) => {
+    console.log("this is a deletion");
+  });
 };
 
 const allListItems = document.querySelectorAll(".delete");
 for (let index = 0; index < allListItems.length; index++) {
-  allListItems[index].addEventListener("click", function() {
+  allListItems[index].addEventListener("click", function () {
     // console.log('allListItems id',  allListItems[index].id);
     deleteItem(allListItems[index].id);
     this.classList.toggle("active");
   });
   allListItems[index]
-  .querySelector("#delete")
-  .addEventListener("click", function() {
-    this.closest(".delete").remove();
-  });
+    .querySelector("#delete")
+    .addEventListener("click", function () {
+      this.closest(".delete").remove();
+    });
 }
-
